819c690cfbb7066d137a7514448540d85009b466
João Vitor Marques <jvsilvam@outlook.com>
2024-03-26 11:00:36 -0300

    chore: add server structure

---
+++ server/src/main/java/com/drconvert/drconvert/DrconvertApplication.java
@@ -0,0 +1,13 @@
+package com.drconvert.drconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrconvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrconvertApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/drconvert/drconvert/DrconvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drconvert.drconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrconvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
