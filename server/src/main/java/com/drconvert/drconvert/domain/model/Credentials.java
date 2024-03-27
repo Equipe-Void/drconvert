7995cbe54553ed5c932d48b5f7f50ab321e877ad
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2024-03-27 20:55:25 -0300

    feat: credentials domain model

---
+++ server/src/main/java/com/drconvert/drconvert/domain/model/Credentials.java
@@ -0,0 +1,34 @@
+package com.drconvert.drconvert.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Credentials {
+
+  @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", updatable = false, unique = true, nullable = false)
+  private Long id;
+
+  @OneToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "email", unique = true, nullable = false)
+  private String email;
+
+  @Column(name = "password", unique = true, nullable = false)
+  private String password;
+
+}
--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO user (name) VALUES ('Joao');
+
+INSERT INTO credentials (user_id, email, password) VALUES (1,'biel@gmail.com', '123');
