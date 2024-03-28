94e911f3376695b7b79a19e6be20eba0fba2d718
Gabriel Vieira <gabrielvieira.tcc@gmail.com>
2024-03-27 22:29:48 -0300

    docs: add sprint backlog

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,11 +1,3 @@
-INSERT INTO user (name) VALUES ('Joao');
+INSERT INTO user (name, user_type) VALUES ('Admin', 'ADMINISTRATOR');
 
-INSERT INTO credentials (user_id, email, password) VALUES (1,'biel@gmail.com', '123');
-
-INSERT INTO project (user_id, name) values (1,'teste');
-
-INSERT INTO log (user_id, action, project_id) VALUES (1,'mudei',1);
-
-INSERT INTO file (project_id, file_path) values (1,'C:ProgramFiles/LAB3');
-
-INSERT INTO field (name, type, project_id) VALUES ('nome', 'STRING', 1);
+INSERT INTO credentials (user_id, email, password) VALUES (1,'admin@admin.com', '123');
