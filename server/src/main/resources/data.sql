cbc1cab0154fa9f460e86885074788e90a572b8c
João Vitor Marques <jvsilvam@outlook.com>
2024-04-01 11:02:39 -0300

    feat: security filter, config and token service impl

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,3 +1,11 @@
-INSERT INTO user (name, user_type) VALUES ('Admin', 'ADMINISTRATOR');
+INSERT INTO users (email, name, password) VALUES ("email@email.com", "test", "$2a$12$cwzWOOaj61uf4XObiOk8heuaPYlKlgVFsTA5VnkWSfiZx.E3V/OSW");
 
-INSERT INTO credentials (user_id, email, password) VALUES (1,'admin@admin.com', '123');
+INSERT INTO roles (role) VALUES ("CREATE_PROJECT");
+INSERT INTO roles (role) VALUES ("READ");
+INSERT INTO roles (role) VALUES ("UPDATE_FIELD");
+INSERT INTO roles (role) VALUES ("DELETE_FIELD");
+INSERT INTO roles (role) VALUES ("CREATE_FIELD");
+INSERT INTO roles (role) VALUES ("CREATE_USER");
+
+INSERT INTO users_roles (roles_id, user_id) VALUES (6, 1);
+-- INSERT INTO users_roles (roles_id, user_id) VALUES (2, 1);
