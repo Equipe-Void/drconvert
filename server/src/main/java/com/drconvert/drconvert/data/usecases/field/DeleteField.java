0e3fa6eafd50d43699ff4a8c2b2d3b9babe95506
João Vitor Marques <jvsilvam@outlook.com>
2024-04-12 20:20:22 -0300

    feat: delete field controller

--- server/src/main/java/com/drconvert/drconvert/data/usecases/field/DeleteField.java
+++ server/src/main/java/com/drconvert/drconvert/data/usecases/field/DeleteField.java
@@ -1,23 +1,21 @@
 package com.drconvert.drconvert.data.usecases.field;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.drconvert.drconvert.domain.model.Field;
-import com.drconvert.drconvert.domain.usecases.field.FindFieldByIdUseCase;
+import com.drconvert.drconvert.domain.usecases.field.DeleteFieldUseCase;
 import com.drconvert.drconvert.infra.repositories.FieldRepository;
+import com.drconvert.drconvert.presentation.errors.FieldNotFoundException;
 
 @Service
-public class FindFieldById implements FindFieldByIdUseCase {
+public class DeleteField implements DeleteFieldUseCase {
   
   @Autowired
   private FieldRepository fieldRepository;
 
   @Override
-  public Optional<Field> find(Long fieldId) {
-    return this.fieldRepository.findById(fieldId);
+  public void delete(Long fieldId) throws FieldNotFoundException {
+    this.fieldRepository.deleteById(fieldId);
   }
 
 }
--- server/src/main/java/com/drconvert/drconvert/presentation/controllers/field/DeleteFieldController.java
+++ server/src/main/java/com/drconvert/drconvert/presentation/controllers/field/DeleteFieldController.java
@@ -1,16 +1,23 @@
 package com.drconvert.drconvert.presentation.controllers.field;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.drconvert.drconvert.domain.model.Field;
 import com.drconvert.drconvert.domain.usecases.field.DeleteFieldUseCase;
+import com.drconvert.drconvert.domain.usecases.field.FindFieldByIdUseCase;
+import com.drconvert.drconvert.presentation.errors.BadRequestException;
 import com.drconvert.drconvert.presentation.errors.FieldNotFoundException;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class DeleteFieldController {
@@ -18,16 +25,22 @@ public class DeleteFieldController {
   @Autowired
   private DeleteFieldUseCase deleteField;
 
+  @Autowired
+  private FindFieldByIdUseCase findFieldById;
+
   @DeleteMapping("/fields/{fieldId}")
   public ResponseEntity<Void> deleteField(@PathVariable Long fieldId) {
-    // Tenta deletar o campo com o ID fornecido
+    Optional<Field> fieldExists = this.findFieldById.find(Long.valueOf(fieldId));
+
+    if(!fieldExists.isPresent()) {
+      throw new FieldNotFoundException();
+    }
+
     try {
-      deleteField.delete(fieldId);
-      return ResponseEntity.noContent().build(); // Retorna uma resposta vazia com status 204 (No Content) se bem-sucedido
+      this.deleteField.delete(Long.valueOf(fieldId));
+      return ResponseEntity.noContent().build();
     } catch (FieldNotFoundException e) {
-      return ResponseEntity.notFound().build(); // Retorna uma resposta com status 404 (Not Found) se o campo não for encontrado
-    } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // Retorna uma resposta com status 500 (Internal Server Error) se ocorrer um erro interno
+      throw new BadRequestException("Erro ao deletar campo");
     }
   }
 }
