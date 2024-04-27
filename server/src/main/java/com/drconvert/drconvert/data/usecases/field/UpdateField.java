f00a19667a923a161afcdb4b443787863e831889
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2024-04-26 22:34:06 -0300

    feat: impl update field with classification

--- server/src/main/java/com/drconvert/drconvert/data/usecases/field/UpdateField.java
+++ server/src/main/java/com/drconvert/drconvert/data/usecases/field/UpdateField.java
@@ -1,8 +1,11 @@
 package com.drconvert.drconvert.data.usecases.field;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.drconvert.drconvert.domain.model.Classification;
 import com.drconvert.drconvert.domain.model.Field;
 import com.drconvert.drconvert.domain.usecases.field.UpdateFieldUseCase;
 import com.drconvert.drconvert.infra.repositories.FieldRepository;
@@ -15,11 +18,15 @@ public class UpdateField implements UpdateFieldUseCase {
   private FieldRepository fieldRepository;
 
   @Override
-  public Field update(Field oldField, UpdateFieldRequestDTO data) {
+  public Field update(Field oldField, UpdateFieldRequestDTO data, Optional<Classification> classification) {
     oldField.setName(data.name());
     oldField.setType(data.type());
     oldField.setIsIdentifier(data.isIdentifier());
     oldField.setIsNullable(data.isNullable());
+
+    if (classification.isPresent()) {
+      oldField.setClassification(classification.get());
+    }
 
     return fieldRepository.save(oldField);
   }
--- server/src/main/java/com/drconvert/drconvert/domain/usecases/field/UpdateFieldUseCase.java
+++ server/src/main/java/com/drconvert/drconvert/domain/usecases/field/UpdateFieldUseCase.java
@@ -1,10 +1,13 @@
 package com.drconvert.drconvert.domain.usecases.field;
 
+import java.util.Optional;
+
+import com.drconvert.drconvert.domain.model.Classification;
 import com.drconvert.drconvert.domain.model.Field;
 import com.drconvert.drconvert.presentation.dto.field.UpdateFieldRequestDTO;
 
 public interface UpdateFieldUseCase {
 
-  Field update(Field oldField, UpdateFieldRequestDTO data);
+  Field update(Field oldField, UpdateFieldRequestDTO data, Optional<Classification> classification);
 
 }
