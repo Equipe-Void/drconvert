fcd0c7ed0aafdaeeb82d398a7da007870e14e6f7
DevBielgrazi <gabrielvieira.tcc@gmail.com>
2024-05-02 20:49:00 -0300

    refactor: remove unused imports

--- server/src/main/java/com/drconvert/drconvert/domain/usecases/classification/DeleteClassificationUseCase.java
+++ server/src/main/java/com/drconvert/drconvert/domain/usecases/classification/DeleteClassificationUseCase.java
@@ -24,24 +24,17 @@ public class DeleteClassificationUseCase {
 
     @Autowired
     private FindUserByIdUseCase findUserByIdUseCase;
-    
+
     public void delete(Long classificationId) {
-        // Verifica se a classificação existe
         Classification classification = classificationRepository.findById(classificationId)
                 .orElseThrow(ClassificationNotFoundException::new);
 
-        // Aqui você pode verificar a associação com um usuário, se necessário
-        // Por exemplo, se uma classificação sempre deve estar associada a um usuário
-        // você pode fazer uma verificação aqui
-        // Verifica se o usuário associado à classificação existe
         Optional<User> userExists = findUserByIdUseCase.find(classification.getUser().getId());
         if (!userExists.isPresent()) {
             throw new UserNotFoundException();
         }
 
-        // Exclui a classificação
         classificationRepository.delete(classification);
     }
 
-   
 }
--- server/src/main/java/com/drconvert/drconvert/presentation/controllers/classification/DeleteClassificationController.java
+++ server/src/main/java/com/drconvert/drconvert/presentation/controllers/classification/DeleteClassificationController.java
@@ -1,10 +1,7 @@
 package com.drconvert.drconvert.presentation.controllers.classification;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.drconvert.drconvert.domain.model.Classification;
-import com.drconvert.drconvert.domain.model.User;
 import com.drconvert.drconvert.domain.usecases.classification.DeleteClassificationUseCase;
-import com.drconvert.drconvert.domain.usecases.user.FindUserByIdUseCase;
 import com.drconvert.drconvert.presentation.errors.ClassificationNotFoundException;
-import com.drconvert.drconvert.presentation.errors.UserNotFoundException;
 import com.drconvert.drconvert.infra.repositories.ClassificationRepository;
 
 @CrossOrigin("*")
@@ -30,14 +24,11 @@ public class DeleteClassificationController {
     @Autowired
     private ClassificationRepository classificationRepository;
 
-        @DeleteMapping("/classifications/{id}")
-        public ResponseEntity<?> deleteClassification(@PathVariable("id") Long id) {
+    @DeleteMapping("/classifications/{id}")
+    public ResponseEntity<?> deleteClassification(@PathVariable("id") Long id) {
         try {
-            // Verifica se a classificação existe
             Classification classification = classificationRepository.findById(id)
                     .orElseThrow(ClassificationNotFoundException::new);
-
-            // Chama o caso de uso para excluir a classificação
             deleteClassificationUseCase.delete(id);
 
             return ResponseEntity.ok().build();
