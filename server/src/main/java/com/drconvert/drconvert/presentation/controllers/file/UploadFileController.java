192301b43d94285c65edd78e65be8f94df6cc457
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2024-06-15 17:36:28 -0300

    Update: UploadFileController
    Implementação dos logs na ação de upload de arquivo

--- server/src/main/java/com/drconvert/drconvert/presentation/controllers/file/UploadFileController.java
+++ server/src/main/java/com/drconvert/drconvert/presentation/controllers/file/UploadFileController.java
@@ -3,6 +3,9 @@ package com.drconvert.drconvert.presentation.controllers.file;
 import java.util.List;
 import java.util.Optional;
 
+import com.drconvert.drconvert.data.usecases.user.FindUserById;
+import com.drconvert.drconvert.domain.model.User;
+import com.drconvert.drconvert.domain.usecases.file.LogService;
 import com.drconvert.drconvert.domain.model.Project;
 import com.drconvert.drconvert.domain.usecases.file.MapsHeaderUseCase;
 import com.drconvert.drconvert.domain.usecases.file.UploadFileUseCase;
@@ -29,6 +32,12 @@ public class UploadFileController {
   @Autowired
   private FindProjectByIdUseCase findProjectById;
 
+  @Autowired
+  private LogService logService;
+
+  @Autowired
+  private FindUserById findUserById;
+
   @PostMapping("/upload")
   public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file,
       @RequestParam("projectId") Long projectId, @RequestParam String separator, @RequestParam String hasHeader) {
@@ -42,15 +51,24 @@ public class UploadFileController {
       throw new ProjectNotFoundException();
     }
 
-    try {
-      String filePath = this.uploadFile.upload(file, Long.valueOf(projectId), Boolean.valueOf(hasHeader), projectExists.get());
+    Optional<User> userOptional = this.findUserById.find(projectId);
 
-      List<String> headers = this.mapsHeader.maps(filePath, separator);
+    if (userOptional.isPresent()) {
+      User user = userOptional.get();
+      try {
+        String filePath = this.uploadFile.upload(file, Long.valueOf(projectId), Boolean.valueOf(hasHeader), projectExists.get());
 
-      return ResponseEntity.status(201).body(headers);
-    } catch (Exception e) {
-      throw new BadRequestException("Erro ao fazer upload deste arquivo");
+        List<String> headers = this.mapsHeader.maps(filePath, separator);
+        logService.logAction(user, "upload", projectExists.get());
+
+        return ResponseEntity.status(201).body(headers);
+      } catch (Exception e) {
+        logService.logAction(user, "erro no upload", projectExists.get());
+        throw new BadRequestException("Erro ao fazer upload deste arquivo");
+      }
+    } else {
+      throw new BadRequestException("Usuário não encontrado");
     }
-  }
 
+  }
 }
